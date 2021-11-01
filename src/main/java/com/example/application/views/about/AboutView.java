91f51942eb92b1a9ab62ed26fbabea9e67bb252e
Enver Haase <enver@vaadin.com>
2021-11-01 13:33:06 +0100

    sho

--- src/main/java/com/example/application/views/about/AboutView.java
+++ src/main/java/com/example/application/views/about/AboutView.java
@@ -1,31 +1,109 @@
 package com.example.application.views.about;
 
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Paragraph;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.router.Route;
-import com.vaadin.flow.router.PageTitle;
-import com.example.application.views.MainLayout;
+import java.util.Vector;
 
-@PageTitle("About")
-@Route(value = "about", layout = MainLayout.class)
+import javax.validation.constraints.NotNull;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.gridpro.EditColumnConfigurator;
+import com.vaadin.flow.component.gridpro.GridPro;
+import com.vaadin.flow.component.gridpro.ItemUpdater;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.data.provider.ListDataProvider;
+import com.vaadin.flow.function.ValueProvider;
+import com.vaadin.flow.router.Route;
+
+
+@Route(value = "about")
 public class AboutView extends VerticalLayout {
 
     public AboutView() {
-        setSpacing(false);
+        GridPro<Foo> grid = new GridPro<>();
+        // Grid<Foo> grid = new Grid<>();
+        grid.addColumn(Foo::getBar).setHeader("Bar");
 
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
-        img.setWidth("200px");
-        add(img);
 
-        add(new H2("This place intentionally left empty"));
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+        final Select<String> stringSelect = new Select<>();
+        Vector<String>values = new Vector<>();
+        values.add("eins");
+        values.add("zwei");
+        values.add("drei");
+        values.add("vier");
 
-        setSizeFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        getStyle().set("text-align", "center");
+        Vector<String>values2 = new Vector<>();
+        values2.add("one");
+        values2.add("two");
+        values2.add("three");
+        values2.add("four");
+        values2.add("five");
+
+        stringSelect.setItems(values);
+
+        ValueProvider<Foo, String> valueProv = new ValueProvider<Foo, String>() {
+            private static final long serialVersionUID = 4384938746145675867L;
+
+            @Override
+            public String apply(Foo source) {
+                return source.getBar();
+            }
+        };
+
+        ItemUpdater<Foo,String>itemUpdater = new ItemUpdater<Foo, String>() {
+
+            private static final long serialVersionUID = -4715860794955863805L;
+
+            @Override
+            public void accept(Foo item, String newValue) {
+                item.setBar(newValue);
+            }
+        };
+
+        EditColumnConfigurator<Foo> editCol =  grid.addEditColumn(valueProv);
+        editCol.custom(stringSelect, itemUpdater);
+
+        EditColumnConfigurator<Foo> editCol2 =  grid.addEditColumn(valueProv);
+        editCol2.select(itemUpdater, values2);
+
+        Vector<Foo>testItems = new Vector<>();
+        testItems.add(new Foo("Click me"));
+        testItems.add(new Foo("Click you"));
+        testItems.add(new Foo("Click us"));
+
+        ListDataProvider<String> myDataProvider = new ListDataProvider<>(values);
+        stringSelect.setDataProvider(myDataProvider);
+
+        Button b = new Button("Switch Items " , e -> {
+            if(switchVar) {
+                stringSelect.setItems(values);
+                switchVar=false;
+            }else{
+                stringSelect.setItems(values2);
+                switchVar=true;
+            }
+        });
+
+        add(grid, b);
+
+        grid.setItems(testItems);
     }
 
+    boolean switchVar = false;
+
+    private static class Foo {
+
+        private String bar;
+
+        private Foo(@NotNull String bar) {
+            this.bar = bar;
+        }
+
+        public String getBar() {
+            return bar;
+        }
+
+        public void setBar(String newValue) {
+            this.bar = newValue;
+        }
+    }
 }
--- src/main/java/com/example/application/views/helloworld/HelloWorldView.java
+++ src/main/java/com/example/application/views/helloworld/HelloWorldView.java
@@ -4,6 +4,7 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.PageTitle;
@@ -18,15 +19,32 @@ public class HelloWorldView extends HorizontalLayout {
     private TextField name;
     private Button sayHello;
 
+    private boolean sw = false;
+
     public HelloWorldView() {
         setMargin(true);
         name = new TextField("Your name");
         sayHello = new Button("Say hello");
-        add(name, sayHello);
+
+        Select<String> sel = new Select<>();
+        sel.setItems("eins", "zwei");
+
+        Button b = new Button("Switch select's items", e -> {
+            if (sw) {
+                sel.setItems("one", "two", "three");
+            }
+            else{
+                sel.setItems("rouge", "maron");
+            }
+           sw = !sw;
+        });
+
+        add(name, sayHello, sel, b);
         setVerticalComponentAlignment(Alignment.END, name, sayHello);
         sayHello.addClickListener(e -> {
             Notification.show("Hello " + name.getValue());
         });
+
     }
 
 }
